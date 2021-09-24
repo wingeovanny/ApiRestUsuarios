c3e1202d88f8b41761c78eb8cbe0fcaee63d29f3
Edwin <win_geovanny88@hotmail.com>
2021-09-24 07:29:48 -0500

    Inicio de proyecto

---
+++ src/main/java/com/ebucay/ApiRestH2/SpringbootMiPrimerApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.ebucay.ApiRestH2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMiPrimerApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMiPrimerApiRestApplication.class, args);
+	}
+
+}
